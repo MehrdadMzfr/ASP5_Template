83e9465d77952aa4108643db4c6d064a8cb5efea
sharpiro <dsharpbb09@gmail.com>
2015-09-11 20:46:14 -0400

    Getting Token But Token not working for getting data.

--- src/ASP5.Template.Web/Api/ValuesController.cs
+++ src/ASP5.Template.Web/Api/ValuesController.cs
@@ -1,10 +1,12 @@
 ﻿using ASP5.Template.Core;
 using ASP5.Template.Core.Models;
 using ASP5.Template.Web.Models;
+using Microsoft.AspNet.Authorization;
 using Microsoft.AspNet.Mvc;
 
 namespace ASP5.Template.Web.Api
 {
+    //[Route("[controller]")]
     public class ValuesController : Controller
     {
         private readonly IBusinessService _businessLayer;
@@ -21,6 +23,14 @@ namespace ASP5.Template.Web.Api
         {
             var manufacturers = _efBusinessLayer.GetData();
             return Ok(manufacturers);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult GetSecuredData()
+        {
+            var data = "this is secured data, congratulations";
+            return Ok(data);
         }
     }
 }
--- src/ASP5.Template.Web/Startup.cs
+++ src/ASP5.Template.Web/Startup.cs
@@ -1,12 +1,19 @@
-﻿using ASP5.Template.Core;
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens;
+using System.Security.Cryptography;
+using AspNet.Security.OpenIdConnect.Server;
+using ASP5.Template.Core;
 using ASP5.Template.Web.Models;
 using ASP5.Template.Web.Middleware;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
 using Microsoft.Dnx.Runtime;
 using Microsoft.Framework.Configuration;
 using Microsoft.Framework.DependencyInjection;
-
+using System.Reflection;
+using ASP5.Template.Core.Providers;
 
 namespace ASP5.Template.Web
 {
@@ -16,31 +23,72 @@ namespace ASP5.Template.Web
 
         public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv)
         {
-            var builder = new ConfigurationBuilder(appEnv.ApplicationBasePath).AddJsonFile("config.json").AddEnvironmentVariables();
+            var builder = new ConfigurationBuilder(appEnv.ApplicationBasePath)
+                .AddJsonFile("config.json").AddEnvironmentVariables()
+                .AddJsonFile($"config.{env.EnvironmentName}.json", optional: true);
             Configuration = builder.Build();
         }
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
             var connectionString = Configuration["Data:DefaultConnection:ConnectionString"];
             services.AddInstance<IDataLayer>(new DataLayer(connectionString));
             services.AddTransient<IBusinessService, BusinessService>();
             services.AddTransient<EfBusinessLayer>();
             Context.ConnectionString = connectionString;
+            services.AddAuthentication();
+            services.AddCaching();
+            services.AddMvc();
         }
 
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IRuntimeEnvironment env)
         {
 #if !DEBUG
             app.UseForceSSL();
 #endif
-            app.UseDefaultFiles();
-            app.UseMvc(builder =>
+            app.Map("/api", api =>
             {
-                //builder.MapRoute(name: "defaultRouter", template: "{controller=Home}/{action=Index}");
-                builder.MapRoute(name: "defaultApi", template: "api/{controller}/{action}");
+                api.UseOAuthBearerAuthentication(options =>
+                {
+                    options.AutomaticAuthentication = true;
+                    //options.SecurityTokenValidators = new List<ISecurityTokenValidator> { new };
+                    options.Authority = "http://localhost:59849/";
+                    options.Audience = "http://localhost:59849/";
+                });
+                api.UseMvc(builder =>
+                {
+                    builder.MapRoute(name: "defaultApi", template: "{controller}/{action}");
+                });
             });
+
+            app.UseOpenIdConnectServer(options =>
+            {
+                options.AuthenticationScheme = OpenIdConnectDefaults.AuthenticationScheme;
+
+                if (string.Equals(env.RuntimeType, "Mono", StringComparison.OrdinalIgnoreCase))
+                {
+                    var rsaCryptoServiceProvider = new RSACryptoServiceProvider(2048);
+                    var rsaParameters = rsaCryptoServiceProvider.ExportParameters(true);
+
+                    options.UseKey(new RsaSecurityKey(rsaParameters));
+                }
+                else
+                {
+                    var temp = typeof(Startup).GetTypeInfo().Assembly;
+                    options.UseCertificate(typeof(Startup).GetTypeInfo().Assembly, "ASP5.Template.Web.Certificate.pfx", "Owin.Security.OpenIdConnect.Server");
+                }
+
+                options.ApplicationCanDisplayErrors = true;
+                options.AllowInsecureHttp = true;
+                options.Issuer = new Uri("http://localhost:59849");
+                options.TokenEndpointPath = new PathString("/token");
+                //options.AuthorizationEndpointPath = new PathString("/token");
+                options.AccessTokenLifetime = new TimeSpan(0, 15, 0);
+                options.ValidationEndpointPath = PathString.Empty;
+                options.Provider = new DefaultAuthorizationProvider();
+            });
+
+            app.UseDefaultFiles();
             app.UseStaticFiles();
         }
     }
